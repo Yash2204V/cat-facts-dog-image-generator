4800d7cd8d776b7a3976115a909a570440eddd49
Yash Varma <yash.v220104@gmail.com>
2024-06-04 22:00:46 +0530

    add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public')) 
 // This serves as a static file, the files are in the public folder/directory.
@@ -8,7 +8,6 @@ app.use(express.static('public'))
 app.listen(port, () => {
     console.log(`Example app listening on port  http://localhost:${port}/`)
 });
-
 
 app.get('/animalname',async (request,response)=>{
   const fetchData = await fetch('https://catfact.ninja/fact');
